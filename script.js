d267e1517cd5ae2fb57fdd4c0d9c610d05e57b2c
Anvison <123312164+Anvison@users.noreply.github.com>
2024-09-20 20:31:39 +0530

    script.js added

---
+++ script.js
@@ -0,0 +1,7 @@
+function showPopUp(carId) {
+    document.getElementById(carId).classList.add('active');
+}
+
+function hidePopUp(carId) {
+    document.getElementById(carId).classList.remove('active');
+}
